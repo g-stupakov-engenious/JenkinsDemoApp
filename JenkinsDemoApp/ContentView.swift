f54eabe0bd56fae774be088ed4a1ad3ab468a739
George-engenious <gstupakov@engenious.io>
2025-07-11 01:52:07 +0200

    Add updates

--- JenkinsDemoApp/ContentView.swift
+++ JenkinsDemoApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Jenkins world!")
+            Text("Hello, world!")
         }
         .padding()
     }
